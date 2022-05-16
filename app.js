fc4e7204d18372e9aef2a5b01fe707bf65f359cc
MarinaBzM <102045569+MarinaBzM@users.noreply.github.com>
2022-05-16 07:14:57 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+const servidor = require('./config/servidor')
+const app = servidor.app
+const porta = servidor.porta
+
+const index = require('./routes/index')(app)
+
+app.listen(porta)
---
+++ config/servidor.js
@@ -0,0 +1,9 @@
+//importar o express
+const express = require('express')
+
+const app = express()
+
+const porta = process.env.PORT || 3030
+
+
+module.exports = {app,porta}
